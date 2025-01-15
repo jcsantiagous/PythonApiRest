59378f763242818dcff46fd573dded1d0ff86912
Juan Carlos Santiago <jcsantiagous@gmail.com>
2025-01-15 16:40:23 -0600

    correccion
    correcion de ejecucion de codigo

--- ApiRest/security.py
+++ ApiRest/security.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
 from typing import Optional
+
+from fastapi import HTTPException
+from fastapi.security import HTTPBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-from fastapi import HTTPException, Security
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from config import settings
 
@@ -19,9 +20,9 @@ def get_password_hash(password: str) -> str:
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
